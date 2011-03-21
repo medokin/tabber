9b2e706e0d7e66fb95cbbe410177975ae54ecca2
Nikodem Jaworski <nikodemjawroski@gmail.com>
2011-03-21 08:25:00 +0100

    it works

--- js/plugins.js
+++ js/plugins.js
@@ -8,14 +8,47 @@
 		
 		return this.each(function(){
 			
-			// The "this" points to the current select element:
-			
 			var tabber = $(this);
+			
+			var i = 1;
+			
 			tabber.children('.control').css('display', 'block').addClass(options.control);
+			
+			tabber.children('.control').children('li:first').addClass('active');
+			
 			tabber.children('.control').children('li').each(function(){
-				$(this).text('test');
+				$(this).attr('data-tab', 'tab'+i);
+				i=i+1;
 			});
 			
+			i=1;
+			
+			tabber.children('.tab').each(function(){
+				if(i!=1)
+					$(this).hide();
+				$(this).attr('data-tab', 'tab'+i);
+				i=i+1;
+			});	
+			
+			tabber.children('.control').children('li').click(function(){
+				if(!$(this).hasClass('active')){
+					
+					tabber.children('.control').children('li').removeClass('active');
+					
+					$(this).addClass('active');
+					
+					tabber.children('.tab').hide();	
+					
+					var tab = $(this).attr('data-tab');
+					
+					tabber.children('.tab').each(function(){
+						if($(this).attr('data-tab') == tab)
+							$(this).show();
+					});	
+							
+					
+				}
+			});		
 					
 		});
 	}
